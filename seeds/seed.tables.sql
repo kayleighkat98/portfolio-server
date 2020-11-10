8230caabc0dfe02fea6830929577cae0f40d3c3e
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-11-09 20:25:47 -0600

    initialize

---
+++ seeds/seed.tables.sql
@@ -0,0 +1,13 @@
+BEGIN;
+TRUNCATE 
+    "user";
+INSERT INTO "user" ("id", "username", "name", "password")
+    VALUES
+    (
+        1,
+        'kay',
+        'kayleigh',
+        -- password = "pass"
+        '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+    );
+COMMIT;
---
+++ src/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const userRouter = require("./user/user-router");
+const authRouter = require("./auth/auth-router");
+const app = express();
+//MIDDLEWARE//
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test",
+  })
+);
+app.use(cors());
+app.use(helmet());
+//ROUTES//
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+//ERRORS//
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    //basic error on production
+    response = { error: { message: 'server error' } };
+  } else {
+    //more complex error for development
+    response = { message: error.message, error };
+  }
+  console.error(error);
+  res.status(500).json(response);
+});
+module.exports = app;
